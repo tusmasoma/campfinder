fa7cddb7a6c62281d70ff71ab31b1734de290dec
tusmasoma <s.k18@icloud.com>
2024-01-31 01:31:47 +0900

    image周りをレイヤードアーキテクチャに変更・テストも追加

---
+++ domain/model/image.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Image struct {
+	ID      uuid.UUID
+	SpotID  uuid.UUID
+	UserID  uuid.UUID
+	URL     string
+	Created time.Time
+}
---
+++ usecase/auth.go
@@ -0,0 +1,43 @@
+//go:generate mockgen -source=$GOFILE -package=mock -destination=./mock/$GOFILE
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tusmasoma/campfinder/domain/model"
+	"github.com/tusmasoma/campfinder/domain/repository"
+)
+
+type AuthUseCase interface {
+	FetchUserFromContext(ctx context.Context) (*model.User, error)
+}
+
+type authUseCase struct {
+	ur repository.UserRepository
+}
+
+func NewAuthUseCase(ur repository.UserRepository) AuthUseCase {
+	return &authUseCase{
+		ur: ur,
+	}
+}
+
+type ContextKey string
+
+const ContextUserIDKey ContextKey = "userID"
+
+func (auc *authUseCase) FetchUserFromContext(ctx context.Context) (*model.User, error) {
+	userID, ok := ctx.Value(ContextUserIDKey).(string)
+	if !ok {
+		log.Printf("Failed to retrieve userId from context")
+		return nil, fmt.Errorf("user name not found in request context")
+	}
+	user, err := auc.ur.GetUserByID(ctx, userID)
+	if err != nil {
+		log.Printf("Failed to get UserInfo from db: %v", userID)
+		return nil, err
+	}
+	return &user, nil
+}
