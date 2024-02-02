ef6f0db837375ba578b70cde2cdc48a743a2e3a7
tusmasoma <s.k18@icloud.com>
2024-02-02 16:40:04 +0900

    dbのユニットテストをdockertestライブラリを使用して実装

---
+++ infra/common_test.go
@@ -0,0 +1,106 @@
+package infra
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+
+	_ "github.com/go-sql-driver/mysql" // This blank import is used for its init function
+)
+
+// Start はDockerを使用してMySQLコンテナを起動し、データベース接続を確立する関数です。
+func Start() (*sql.DB, func(), error) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current directory: %s", err)
+	}
+	log.Print(pwd)
+
+	// Dockerのデフォルト接続方法を使用（Windowsではtcp/http、Linux/OSXではsocket）
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Printf("Could not construct pool: %s\n", err)
+		return nil, nil, err
+	}
+
+	// Dockerに接続を試みる
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Printf("Could not connect to Docker: %s", err)
+		return nil, nil, err
+	}
+
+	// Dockerコンテナを起動する際に指定する設定定義
+	runOptions := &dockertest.RunOptions{
+		Repository: "mysql",
+		Tag:        "5.7",
+		Env: []string{
+			"MYSQL_ROOT_USERNAME=root",
+			"MYSQL_ROOT_PASSWORD=campfinder",
+			"MYSQL_DATABASE=campfinderdb",
+		},
+	}
+
+	// runOptions設定を適用してDockerコンテナを起動します。成功するとresourceは、起動したコンテナを表す。
+	resource, err := pool.RunWithOptions(runOptions,
+		func(hc *docker.HostConfig) {
+			hc.AutoRemove = true
+			hc.RestartPolicy = docker.RestartPolicy{
+				Name: "no",
+			}
+			hc.Mounts = []docker.HostMount{
+				{
+					Type:   "bind",
+					Source: pwd + "/init/ddl.sql",
+					Target: "/docker-entrypoint-initdb.d/ddl.sql",
+				},
+			}
+		},
+	)
+	if err != nil {
+		log.Printf("Could not start resource: %s", err)
+		return nil, nil, err
+	}
+
+	port := resource.GetPort("3306/tcp")
+
+	// データベース接続が成功するまで定期的に接続試行を行うことを試みる(待機)
+	var db *sql.DB
+	err = pool.Retry(func() error {
+		dsn := fmt.Sprintf("root:campfinder@(localhost:%s)/campfinderdb?timeout=30s", port)
+		db, err = sql.Open("mysql", dsn)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	})
+
+	if err != nil {
+		log.Printf("Could not connect to docker: %s", err)
+		return nil, nil, err
+	}
+
+	log.Println("start MySQL container🐳")
+
+	// // データベース接続とクリーンアップ関数を返却
+	return db, func() { closeMySQL(db, pool, resource) }, nil
+}
+
+// closeMySQL はMySQLデータベースの接続を閉じ、Dockerコンテナを停止・削除する関数
+func closeMySQL(db *sql.DB, pool *dockertest.Pool, resource *dockertest.Resource) {
+	// データベース接続を切断
+	if err := db.Close(); err != nil {
+		log.Fatalf("Failed to close database: %s", err)
+	}
+
+	// Dockerコンテナを停止して削除
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Failed to purge resource: %s", err)
+	}
+
+	log.Println("close MySQL container🐳")
+}
