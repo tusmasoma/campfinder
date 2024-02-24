d9a838b994b38e233710403fa5b043e2bb921b2c
tusmasoma <s.k18@icloud.com>
2024-02-24 20:15:34 +0900

    サーバーに関しての環境変数をgo-envconfig ライブラリを使って読み込む#48

--- docker/back/config/server.go
+++ docker/back/config/server.go
@@ -1,15 +1,5 @@
 package config
 
-import "time"
-
-const (
-	ReadTimeout                   = 5 * time.Second
-	WriteTimeout                  = 10 * time.Second
-	IdleTimeout                   = 15 * time.Second
-	GracefulShutdownTimeout       = 5 * time.Second
-	PreflightCacheDurationSeconds = 300
-)
-
 type ContextKey string
 
 const ContextUserIDKey ContextKey = "userID"
