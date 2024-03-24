6b9bc1cd338b5ad49ae5230f2236523f6cdba2c9
tusmasoma <s.k18@icloud.com>
2024-03-24 18:42:17 +0900

    create base.go

---
+++ docker/back/infra/init/ddl.test.sql
@@ -0,0 +1,12 @@
+USE `campfinderdb` ;
+
+DROP TABLE IF EXISTS TestItems CASCADE;
+
+CREATE TABLE TestItems (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    user_id CHAR(36) NOT NULL,
+    text TEXT NOT NULL,
+    count INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
