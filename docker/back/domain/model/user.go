e5a6e92b584a46ff72adef6db848f70a50574f8e
tusmasoma <s.k18@icloud.com>
2024-04-02 13:30:20 +0900

    refactor

--- docker/back/domain/model/user.go
+++ docker/back/domain/model/user.go
@@ -3,9 +3,9 @@ package model
 import "github.com/google/uuid"
 
 type User struct {
-	ID       uuid.UUID
-	Name     string
-	Email    string
-	Password string // ハッシュ化されたパスワード
-	IsAdmin  bool
+	ID       uuid.UUID `db:"id"`
+	Name     string    `db:"name"`
+	Email    string    `db:"email"`
+	Password string    `db:"password"` // ハッシュ化されたパスワード
+	IsAdmin  bool      `db:"is_admin"`
 }
