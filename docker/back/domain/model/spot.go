acb6716674f78ec6a6fd6a1fc0b7bf56f38f9ae3
tusmasoma <s.k18@icloud.com>
2024-04-02 09:41:26 +0900

    refactor

--- docker/back/domain/model/spot.go
+++ docker/back/domain/model/spot.go
@@ -3,15 +3,15 @@ package model
 import "github.com/google/uuid"
 
 type Spot struct {
-	ID          uuid.UUID
-	Category    string  `json:"category"`
-	Name        string  `json:"name"`
-	Address     string  `json:"address"`
-	Lat         float64 `json:"lat"`
-	Lng         float64 `json:"lng"`
-	Period      string  `json:"period"`
-	Phone       string  `json:"phone"`
-	Price       string  `json:"price"`
-	Description string  `json:"description"`
-	IconPath    string  `json:"iconpath"`
+	ID          uuid.UUID `db:"id"`
+	Category    string    `db:"category" json:"category"`
+	Name        string    `db:"name" json:"name"`
+	Address     string    `db:"address" json:"address"`
+	Lat         float64   `db:"lat" json:"lat"`
+	Lng         float64   `db:"lng" json:"lng"`
+	Period      string    `db:"period" json:"period"`
+	Phone       string    `db:"phone" json:"phone"`
+	Price       string    `db:"price" json:"price"`
+	Description string    `db:"description" json:"description"`
+	IconPath    string    `db:"iconpath" json:"iconpath"`
 }
