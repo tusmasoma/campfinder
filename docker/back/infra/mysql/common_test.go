ad137188fa56c8ba0324afa98621a576203f0d40
tusmasoma <s.k18@icloud.com>
2024-05-19 22:44:23 +0900

    fix: refactor db name in test

--- docker/back/infra/mysql/common_test.go
+++ docker/back/infra/mysql/common_test.go
@@ -56,7 +56,7 @@ func startMySQL() (*sql.DB, func(), error) {
 		Env: []string{
 			"MYSQL_ROOT_USERNAME=root",
 			"MYSQL_ROOT_PASSWORD=campfinder",
-			"MYSQL_DATABASE=campfinderdb",
+			"MYSQL_DATABASE=testdb",
 		},
 		Cmd: []string{
 			"--character-set-server=utf8mb4",
@@ -94,7 +94,7 @@ func startMySQL() (*sql.DB, func(), error) {
 
 	// データベース接続が成功するまで定期的に接続試行を行うことを試みる(待機)
 	err = pool.Retry(func() error {
-		dsn := fmt.Sprintf("root:campfinder@(localhost:%s)/campfinderdb?charset=utf8mb4&parseTime=true", port)
+		dsn := fmt.Sprintf("root:campfinder@(localhost:%s)/testdb?charset=utf8mb4&parseTime=true", port)
 		db, err = sql.Open("mysql", dsn)
 		if err != nil {
 			return err
